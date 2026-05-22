3f4fa5f01356dde208d8ad48f2f9fc7e9699ae43
Cherno <cherno@users.noreply.github.com>
2026-05-22 22:53:30 +0800

    Add ds-mon cask

---
+++ Casks/ds-mon.rb
@@ -0,0 +1,18 @@
+cask "ds-mon" do
+  version "1.0"
+  sha256 :no_check
+
+  url "https://github.com/Cherno76/DS-mon/releases/download/v#{version}/DS-mon-v#{version}.zip"
+  name "DS-mon"
+  desc "macOS menu bar DeepSeek API balance monitor"
+  homepage "https://github.com/Cherno76/DS-mon"
+
+  depends_on macos: ">= :sequoia"
+  depends_on arch: :arm64
+
+  app "DS-mon.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.cherno.DS-mon.plist",
+  ]
+end
